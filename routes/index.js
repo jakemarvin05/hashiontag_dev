a3879bbe3f07db464472b6b972613d25a4e7c418
Calvin Tan <calvin14@gmail.com>
2014-07-18 00:52:31 +0800

    Updated pg example
    updated with pg example in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,40 @@
 var express = require('express');
 var router = express.Router();
-var sys = require('sys');
-
+//var sys = require('sys');
+var pg = require('pg').native;
+var conString = "postgres://achmxwosijzdwc:g_05iNqib65ZR7wZArOjfhkmh7@ec2-54-235-245-180.compute-1.amazonaws.com:5432/d2qhi6e8qpq1js";
 
 
 module.exports = router;
 
 // Homepage
 router.get('/', function (req, res) {
-  sys.puts(sys.inspect(req));
+  //sys.puts(sys.inspect(req));
   res.render('index', { 
     title: 'Hashiontag', 
-    someVariable: 'This is a variable passed from node.js'
+    someVariable: 'We are using:'
   });
+});
+
+router.get('/dbtest', function (req, res) {
+
+var client = new pg.Client(conString);
+
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+    client.end();
+
+    res.send(result.rows[0].theTime);
+  });
+});
+
+  
 });
