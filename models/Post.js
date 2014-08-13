064e0f0b65b012b7ad93356eca1d713e08b69c20
Calvin Tan <calvin14@gmail.com>
2014-08-13 13:20:45 +0800

    13aug rollup
    13aug rollup

--- models/Post.js
+++ models/Post.js
@@ -37,65 +37,133 @@ module.exports = function(sequelize, DataTypes) {
             },
             getterMethods: {
                 timeLapse: function() { 
-                    /* 
-                    TO DO. Work this shit out:
-
-                    1 minute = 60
-                    1 hour = 3600
-                    1 day = 86400
-                    1 week = 604800
-                    (each month has 4.3452 weeks)
-                    1 month = 2 627 977
-                    1 year = 31 536 000
-
-
-                    Thresholds:
-
-                    Less than 60 seconds:
-                        Show Seconds
-
-                    More than 60 seconds, less than
-                    */
 
                     var postTime = moment( this.get('createdAt') );
                     var timeHasLapsed = moment() - postTime;
 
-                    //seconds lapsed
+
                     var secondsLapsed = timeHasLapsed / 1000;
-                    timeHasLapsed = Math.round(secondsLapsed) + ' seconds ago';
 
-                    //minutes lapsed
-                    if (secondsLapsed > 59.5 && secondsLapsed < 3570) {
-                        var minutesHasLapsed = secondsLapsed / 60;
-                        timeHasLapsed = 'about ' + Math.round(minutesHasLapsed) + ' minutes ago';
+                    //1 SECOND lapsed
+                    timeHasLapsed = '1 second ago';
+
+                    //SECONDS lapsed
+                    if (secondsLapsed >= 1.5 && secondsLapsed < 59.5) {
+                        
+                        timeHasLapsed = Math.round(secondsLapsed) + ' seconds ago';
+
+                    return timeHasLapsed;
                     }
 
-                    //hours lapsed, 84600 seconds is 23.5hours
-                    if (secondsLapsed > 3570 && secondsLapsed < 84600) {
-                        var hoursHasLapsed = secondsLapsed / 3600;
-                        timeHasLapsed = 'about ' + Math.round(hoursHasLapsed) + ' hours ago';
+
+                    //MINUTES lapsed, 3570 seconds is 59.5 minutes
+                    if (secondsLapsed >= 59.5 && secondsLapsed < 3570) {
+
+                        var minutesHasLapsed = secondsLapsed / 60,
+                            min = 'minute',
+                            minRnd = Math.round(minutesHasLapsed);
+
+                        if( minRnd > 1 ) {
+                            min = 'minutes';
+                        }
+                        
+                        timeHasLapsed = minRnd + ' ' + min + ' ago';
+                    return timeHasLapsed;
                     }
-                    
-                    //days lapsed, 561600 seconds is 6.5days
-                    if (secondsLapsed > 84600 && secondsLapsed < 561600) {
+
+
+                    //HOURS lapsed, 84600 seconds is 23.5 hours
+                    if (secondsLapsed >= 3750 && secondsLapsed < 84600) {
+
+                        var hoursHasLapsed = secondsLapsed / 3600,
+                                hr = 'hour';
+                                hrRnd = Math.round(hoursHasLapsed);
+
+                        if(hrRnd > 1) {
+                            hr = 'hours';
+                        }
+
+                        timeHasLapsed = hrRnd + ' ' + hr + ' ago';
+
+                    return timeHasLapsed;
+                    }
+
+
+                    //DAYS lapsed,  561600 seconds is 6.5 days
+                    if(secondsLapsed >= 84600 && secondsLapsed < 561600) {
+
                         var daysHasLapsed = secondsLapsed / 86400;
-                        timeHasLapsed = 'about ' + Math.round(daysHasLapsed) + ' days ago';
+                            day = 'day';
+                            dayRnd = Math.round(daysHasLapsed);
+
+                        if(dayRnd > 1) {
+                            day = 'days';
+                        }
+                        timeHasLapsed = dayRnd + ' ' + day + ' ago';
+
+                    return timeHasLapsed;
                     }
 
-                    //weeks lapsed, 2116800 seconds is 3.5weeks
-                    if (secondsLapsed > 561600 && secondsLapsed < 2116800) {
-                        var weeksHasLapsed = secondsLapsed / 604800;
-                        timeHasLapsed = 'about ' + Math.round(weeksHasLapsed) + ' weeks ago';
+
+                    //WEEK lapsed, 2116800 seconds is 3.5weeks
+                    if (secondsLapsed >= 561600 && secondsLapsed < 2116800) {
+                        var weeksHasLapsed = secondsLapsed / 604800,
+                            week = 'week',
+                            weekRnd = Math.round(weeksHasLapsed);
+
+                        if(Math.round(weeksHasLapsed) > 1) {
+                            week = 'weeks';
+                        }
+
+                        timeHasLapsed = weekRnd + ' ' + week + ' ago';
+
+                    return timeHasLapsed;
                     }
 
-                    //months lapsed, 30222011 seconds is about 11.5 months
-                    if (secondsLapsed > 2116800 && secondsLapsed < 30222011) {
-                        var monthsHasLapsed = secondsLapsed / 2627977;
-                        timeHasLapsed = 'about ' + Math.round(monthsHasLapsed) + ' months ago';
+
+                    //MONTHS lapsed, 30221735.499 seconds is about 11.5 months
+                    if (secondsLapsed >= 2116800 && secondsLapsed < 30221735.499) {
+                        var monthsHasLapsed = secondsLapsed / 2627977,
+                            month = 'month',
+                            monthRnd = Math.round(monthsHasLapsed);
+
+                        if(Math.round(monthsHasLapsed) > 1) {
+                            month = 'months';
+                        }
+
+                        timeHasLapsed = monthRnd + ' ' + month + ' ago';
+
+                    return timeHasLapsed;
                     }
 
-                return timeHasLapsed;
-                }
+
+                    //YEAR lapsed, 151110055 seconds is about 5 years
+                    if (secondsLapsed >= 30221735.499 && secondsLapsed < 151110055) {
+                        var yearsHasLapsed = secondsLapsed / 30222011,
+                            year = 'year',
+                            yearRnd = Math.round(yearsHasLapsed);
+
+                        if(yearRnd > 1) {
+                            year = 'years';
+                        }
+
+                        timeHasLapsed = yearRnd + ' ' + year + ' ago';
+
+                    return timeHasLapsed;
+                    }
+
+                 
+                    //more than 5 year lapse
+                    if(secondsLapsed >= 151110055) {
+
+                        //lol
+                        timeHasLapsed = 'Hashiontag is now more than 5 years old! - WY';
+
+                    return timeHasLapsed;
+                    }
+
+                return false;
+                } 
             }
         }
     );
