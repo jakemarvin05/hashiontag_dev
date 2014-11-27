5001df3e449186fbf407618f2162c279ca24266b
Calvin Tan <calvin14@gmail.com>
2014-11-27 21:16:23 +0800

    forget password text improvements

--- apps/mailer/forgotPassword.js
+++ apps/mailer/forgotPassword.js
@@ -5,13 +5,16 @@ module.exports = function send(user, host) {
 
   var resetUrl = host + '/passwordtokenreset' + '?token=' + user.passwordResetToken;
 
+  var html  = '<h3>Hi there!</h3>';
+      html += '<p>You have requested for a password reset. In order to reset your password please follow this <a href="' + resetUrl + '">link</a>.</p>';
+      html += '<p>You have 12 hours before the request expires. If you did not request for the change, please ignore this mail.</p>';
+
   sendgrid.send({
     to:       user.email,
     from:     'info@vogueverve.com',
     fromname: 'Vogue and Verve',
     subject:  'VogueVerve password reset for ' + user.email + '.',
-    html:     'You have request for a password reset. In order to reset your password please follow this <a href="' + resetUrl + '">link</a>.' + '\n\n' +
-              'You have 12 hours before the request expires.\n\nIf you did not request for the change, please ignore this mail.\n\n'
+    html:     html
   }, function(err, json){
     if (err) { return false; }
     return json;
